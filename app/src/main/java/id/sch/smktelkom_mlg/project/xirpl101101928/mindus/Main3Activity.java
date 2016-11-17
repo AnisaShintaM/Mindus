1fe57c9a5c4be2eed378e0981f933728771b642c
Dhenarra Erine Aisyahsari <dhenarra18@gmail.com>
2016-11-17 20:52:57 +0700

    +layout informasi +memfungsikan back press pada halaman tambah dan main3activity +inisialisasi pada tambah_activity.java
    -hapus layout informasi dan mengganti dengan tambah_activity

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl101101928/mindus/Main3Activity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl101101928/mindus/Main3Activity.java
@@ -1,7 +1,10 @@
 package id.sch.smktelkom_mlg.project.xirpl101101928.mindus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main3Activity.this, Tambah_Activity.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl101101928/mindus/Tambah_Activity.java
@@ -0,0 +1,40 @@
+package id.sch.smktelkom_mlg.project.xirpl101101928.mindus;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class Tambah_Activity extends AppCompatActivity {
+
+    EditText etSummary;
+    EditText etDescription;
+    EditText etTanggal;
+    ImageButton ibFoto;
+    Button bTambah;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        etSummary = (EditText) findViewById(R.id.editTextSummary);
+        etDescription = (EditText) findViewById(R.id.editTextDeskripsi);
+        etTanggal = (EditText) findViewById(R.id.editTextTanggal);
+        ibFoto = (ImageButton) findViewById(R.id.imageViewFoto);
+        bTambah = (Button) findViewById(R.id.buttonSimpan);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
