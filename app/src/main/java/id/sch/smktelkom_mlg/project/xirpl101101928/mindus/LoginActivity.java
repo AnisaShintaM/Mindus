c1c5195e1e5c7f9cb8f7576e4c46ffa89a9e2963
RafidiaAR <rafidiaar007@gmail.com>
2016-11-20 18:08:19 +0700

    Firebase Register

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl101101928/mindus/LoginActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl101101928/mindus/LoginActivity.java
@@ -1,13 +1,70 @@
 package id.sch.smktelkom_mlg.project.xirpl101101928.mindus;
 
+import android.annotation.TargetApi;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.transition.Explode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+    @InjectView(R.id.et_email)
+    EditText et_email;
+    @InjectView(R.id.et_password)
+    EditText et_password;
+    @InjectView(R.id.btn_login)
+    Button btn_login;
+    @InjectView(R.id.cv)
+    CardView cv;
+    @InjectView(R.id.fab)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+        ButterKnife.inject(this);
+
     }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @OnClick({R.id.btn_login, R.id.fab})
+    public void OnClick(View view) {
+        switch (view.getId()) {
+            case R.id.fab:
+                getWindow().setExitTransition(null);
+                getWindow().setEnterTransition(null);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options =
+                            ActivityOptions.makeSceneTransitionAnimation(this, fab, fab.getTransitionName());
+                    startActivity(new Intent(this, RegisterActivity.class), options.toBundle());
+                } else {
+                    startActivity(new Intent(this, RegisterActivity.class));
+                }
+                break;
+            case R.id.btn_login:
+                Explode explode = new Explode();
+                explode.setDuration(500);
+
+                getWindow().setExitTransition(explode);
+                getWindow().setEnterTransition(explode);
+                ActivityOptionsCompat oc2 = ActivityOptionsCompat.makeSceneTransitionAnimation(this);
+                Intent i2 = new Intent(this, MainActivity.class);
+                startActivity(i2, oc2.toBundle());
+                break;
+        }
+    }
+
 }
--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl101101928/mindus/RegisterActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl101101928/mindus/RegisterActivity.java
@@ -1,13 +1,82 @@
 package id.sch.smktelkom_mlg.project.xirpl101101928.mindus;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button buttonRegister;
+    private EditText editTextemail;
+    private EditText editTextpassword;
+    private TextView textViewregister;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        buttonRegister = (Button) findViewById(R.id.btn_login);
+        editTextemail = (EditText) findViewById(R.id.editText4);
+        editTextpassword = (EditText) findViewById(R.id.editText5);
+        textViewregister = (TextView) findViewById(R.id.textView9);
+        buttonRegister.setOnClickListener(this);
+        textViewregister.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonRegister) {
+            registeruser();
+        }
+        if (view == textViewregister) {
+            // will open login activity
+        }
+    }
+
+    private void registeruser() {
+        String email = editTextemail.getText().toString().trim();
+        String password = editTextpassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please Enter Email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please ENter Password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressDialog.setMessage("Registering User");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Register Successfully", Toast.LENGTH_SHORT);
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Could not register. Please Try Again", Toast.LENGTH_SHORT);
+
+                        }
+                    }
+                });
     }
 }
